a1e27a36c72e1c2b1e41fdc6b3ed7db455b9be27
fedemunoz <fedemz88@gmail.com>
2014-09-09 01:14:12 -0300

    final

--- leap/src/Main.java
+++ leap/src/Main.java
@@ -11,6 +11,7 @@ import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Robot;
 import java.awt.TextArea;
@@ -21,14 +22,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import static java.lang.System.out;
+import java.net.URL;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.filechooser.FileFilter;
@@ -47,6 +51,8 @@ public class Main extends JFrame{
 	private TextArea output;
         private ActionListener guardarGesto;
         
+        private boolean primeraVez=true;
+        
 	public Main() throws IOException
 	{
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,16 +60,24 @@ public class Main extends JFrame{
             setSize(width, height);
             setBounds(170, 25, width, height);
 		// get rid of annoying layout managers
-//		setLayout(null);
-            JPanel p= new JPanel() {
-                                        BufferedImage img = ImageIO.read(getClass().getResource("fondo.gif"));
-
+//		setLayout(null);    
+            
+            JPanel p2= new JPanel() {
+                                        ImageIcon imageIcon = new ImageIcon(getClass().getResource("fondo.png"));
                                         public void paintComponent(Graphics g) {
-                                          super.paintComponent(g);
-                                          g.drawImage(img, 0, 0, 1040, 680, this);
+                                          Graphics2D g2d = (Graphics2D) g.create();
+                                          g2d.drawImage(imageIcon.getImage(), 0, 0,1040, 680, this);
                                         }
-                                    };
-            setContentPane(p);  
+                                };
+            
+			 JPanel p= new JPanel() {
+											ImageIcon imageIcon = new ImageIcon(getClass().getResource("fondo.gif"));
+											public void paintComponent(Graphics g) {
+											  Graphics2D g2d = (Graphics2D) g.create();
+											  g2d.drawImage(imageIcon.getImage(), 0, 0,1040, 680, this);
+											}
+										};
+			 setContentPane(p);  
              
 		
 		// make the text boxes 
@@ -79,7 +93,7 @@ public class Main extends JFrame{
         
                          
                 //final JButton b2 = new JButton("CHEQUEAR GESTO");
-                JButton b2 = new JButton(new ImageIcon("reconocer.png"));
+                JButton b2 = new JButton(new ImageIcon("reconocerconimagen.png"));
                 b2.setOpaque(false);
                 b2.setContentAreaFilled(false);
                 b2.setBorder(null);
@@ -100,6 +114,17 @@ public class Main extends JFrame{
                 add(b1);
                 b1.setVisible(true);
                 
+                final JButton b4 = new JButton(new ImageIcon("reconocer.png"));
+                b4.setOpaque(false);
+                b4.setVisible(true); 
+                b4.setContentAreaFilled(false);
+                b4.setBorder(null);
+                b4.setBorderPainted(false);
+                b4.setForeground(Color.white);
+                b4.setCursor(new Cursor(Cursor.HAND_CURSOR));                                                
+                b4.setBounds(500, 650, 287, 26);
+                add(b4);
+                
                 //final JButton b3 = new JButton("ELEGIR IMAGEN");
                 JButton b3 = new JButton(new ImageIcon("importar.png"));
                 b3.setOpaque(false);
@@ -110,7 +135,7 @@ public class Main extends JFrame{
                 b3.setCursor(new Cursor(Cursor.HAND_CURSOR));                 
                 b3.setVisible(true);                
                 b3.setBounds(500, 650, 287, 26);
-                add(b3);
+                add(b3);                                
                 
                 final HWRCanvas hwrc = new HWRCanvas(this);
                 hwrc.setBounds(160, 75, 720, 500);
@@ -120,10 +145,15 @@ public class Main extends JFrame{
                     {
                         //Execute when button is pressed
                         hwrc.Grabo=true;
-                        hwrc.init();
+                         hwrc.templateXY= new ArrayList<Integer>();
+                        if(primeraVez){
+                            hwrc.init();       
+                        }
                         hwrc.start();
-
-                        add(hwrc);
+                        if(primeraVez){
+                            add(hwrc);
+                            primeraVez=false;
+                        }
 
                         System.out.println("You clicked the button");
                     }
@@ -133,18 +163,21 @@ public class Main extends JFrame{
                     public void actionPerformed(ActionEvent e)
                     {
                         //Execute when button is pressed
-                         hwrc.Grabo=false;
-                         //hwrc.init();
-                         hwrc.start();
-                         add(hwrc);
-
+                         hwrc.conImagen=true;    
+                         hwrc.Grabo=false;  
+                        
+                         //hwrc.init();                          
+                          hwrc.start();                        
+                         //add(hwrc);
+                         
                         System.out.println("You clicked the button chequear");
+                        //b4.setVisible(true); 
                     }
                 });  
                 
                 b3.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e)
-                    {
+                    {                        
                         JFileChooser filechooser= new JFileChooser();
                         FileFilter imageFilter = new FileNameExtensionFilter("Image files", ImageIO.getReaderFileSuffixes());
                         filechooser.setFileFilter(imageFilter);
@@ -172,6 +205,26 @@ public class Main extends JFrame{
                             
                         }
                     }
+                    
+                    
+                });  
+                
+                b4.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    
+                    //Execute when button is pressed
+                         hwrc.Grabo=false;                         
+                         //hwrc.init();                          
+                          hwrc.start();                        
+                         //add(hwrc);
+                          
+                          hwrc.conImagen=false;
+                                           
+                    
+                    
+                }
+                    
                 });
                 
 		//pack();
@@ -183,291 +236,15 @@ public class Main extends JFrame{
                 
 	}
 	
-	public void detectedA(String gesture, int x, int y, int c_x, int c_y) {
-		System.out.println("This is gestooo...");
+	public void detectedGesto(String gesture, int x, int y, int c_x, int c_y) throws IOException {
+                JOptionPane.showMessageDialog(this, "Has iniciado sesión.");
+               
 		  if(capsOn)
 			output.append("gestoo");
 		  else
 			  output.append("gestooo");
+		System.out.println("This is gestooo...");
 		}
-	
-	public void detectedB(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is B...");
-
-		  if(capsOn)
-			  output.append("B");
-		  else
-			  output.append("b");
-		  System.out.println("output text: " + output.getText());
-		}
-
-	public void detectedC(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is C...");
-
-		  if(capsOn)
-			  output.append("C");
-		  else
-			  output.append("c");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedD(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is D...");
-
-		  if(capsOn)
-			  output.append("D");
-		  else
-			  output.append("d");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedE(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is E...");
-
-		  if(capsOn)
-			  output.append("E");
-		  else
-			  output.append("e");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedF(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is F...");
-
-		  if(capsOn)
-			  output.append("F");
-		  else
-			  output.append("f");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedG(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is G...");
-
-		  if(capsOn)
-			  output.append("G");
-		  else
-			  output.append("g");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedH(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is H...");
-
-		  if(capsOn)
-			  output.append("H");
-		  else
-			  output.append("h");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedI(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is I...");
-
-		  if(capsOn)
-			  output.append("I");
-		  else
-			  output.append("i");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedJ(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is J...");
-
-		  if(capsOn)
-			  output.append("J");
-		  else
-			  output.append("j");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedK(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is K...");
-
-		  if(capsOn)
-			  output.append("K");
-		  else
-			  output.append("k");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedL(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is L...");
-
-		  if(capsOn)
-			  output.append("L");
-		  else
-			  output.append("l");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedM(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is M...");
-
-		  if(capsOn)
-			  output.append("M");
-		  else
-			  output.append("m");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedN(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is N...");
-
-		  if(capsOn)
-			  output.append("N");
-		  else
-			  output.append("n");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedO(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is O...");
-
-		  if(capsOn)
-			  output.append("O");
-		  else
-			  output.append("o");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedP(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is P...");
-
-		  if(capsOn)
-			  output.append("P");
-		  else
-			  output.append("p");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedQ(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is Q...");
-
-		  if(capsOn)
-			  output.append("Q");
-		  else
-			  output.append("q");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedR(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is R...");
-
-		  if(capsOn)
-			  output.append("R");
-		  else
-			  output.append("r");
-		  System.out.println("output text: " + output.getText());
-		}
-
-	public void detectedS(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is S...");
-
-		  if(capsOn)
-			  output.append("S");
-		  else
-			  output.append("s");
-		  System.out.println("output text: " + output.getText());
-		}	
-	
-	public void detectedT(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is T...");
-
-		  if(capsOn)
-			  output.append("T");
-		  else
-			  output.append("t");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedU(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is U...");
-
-		  if(capsOn)
-			  output.append("U");
-		  else
-			  output.append("u");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedV(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is V...");
-
-		  if(capsOn)
-			  output.append("V");
-		  else
-			  output.append("v");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedW(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is W...");
-
-		  if(capsOn)
-			  output.append("W");
-		  else
-			  output.append("w");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedX(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is X...");
-
-		  if(capsOn)
-			  output.append("X");
-		  else
-			  output.append("x");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedY(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is Y...");
-
-		  if(capsOn)
-			  output.append("Y");
-		  else
-			  output.append("y");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedZ(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is Z...");
-
-		  if(capsOn)
-			  output.append("Z");
-		  else
-			  output.append("z");
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	
-	
-
-	public void detectedCaps(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is CAPS...");
-
-		  toggleCaps();
-		  
-		  System.out.println("output text: " + output.getText());
-		}
-	
-	public void detectedSpace(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is space...");
-
-		  output.append(" ");
-		  
-		  System.out.println("output text: " + output.getText());
-	}
-	
-	public void detectedBackSpace(String gesture, int x, int y, int c_x, int c_y) {
-		  System.out.println("This is a backspace...");
-
-		  String txt = output.getText();
-		  output.setText(txt.substring(0, txt.length()-1));
-		  
-		  System.out.println("output text: " + output.getText());
-	}
 	
 	private void toggleCaps()
 	{

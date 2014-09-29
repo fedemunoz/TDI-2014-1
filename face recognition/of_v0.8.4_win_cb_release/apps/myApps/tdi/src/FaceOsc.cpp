ff4c3b0e630232f423a3cea9591a5de113d88bcb
fedemunoz <fedemz88@gmail.com>
2014-09-29 19:11:15 -0300

    panda

--- face recognition/of_v0.8.4_win_cb_release/apps/myApps/tdi/src/FaceOsc.cpp
+++ face recognition/of_v0.8.4_win_cb_release/apps/myApps/tdi/src/FaceOsc.cpp
@@ -11,13 +11,19 @@ void FaceOsc::sendFaceOsc(ofxFaceTracker& tracker) {
         addMessage("/pose/scale", tracker.getScale());
         ofVec3f orientation = tracker.getOrientation();
         addMessage("/pose/orientation", orientation);
-        //addMessage("/joint", tracker.getGesture(ofxFaceTracker::MOUTH_WIDTH),"fede");
+        //leon
         addMessage("/joint", tracker.getGesture(ofxFaceTracker::MOUTH_HEIGHT),"boca");
         addMessage("/joint", tracker.getGesture(ofxFaceTracker::RIGHT_EYEBROW_HEIGHT),"cejaD");
         addMessage("/joint", tracker.getGesture(ofxFaceTracker::RIGHT_EYEBROW_HEIGHT),"altaD");
         addMessage("/joint", tracker.getGesture(ofxFaceTracker::LEFT_EYEBROW_HEIGHT),"cejaI");
         addMessage("/joint", tracker.getGesture(ofxFaceTracker::LEFT_EYEBROW_HEIGHT),"altaI");
-        addMessage("/joint", tracker.getGesture(ofxFaceTracker::JAW_OPENNESS),"nariz");
+
+        //panda
+        addMessage("/joint", tracker.getGesture(ofxFaceTracker::MOUTH_HEIGHT),"boquita");
+        addMessage("/joint", tracker.getGesture(ofxFaceTracker::RIGHT_EYEBROW_HEIGHT),"orejaD");
+        addMessage("/joint", tracker.getGesture(ofxFaceTracker::LEFT_EYEBROW_HEIGHT),"orejaI");
+        addMessage("/joint", tracker.getGesture(ofxFaceTracker::RIGHT_EYE_OPENNESS),"ojoD");
+        addMessage("/joint", tracker.getGesture(ofxFaceTracker::LEFT_EYE_OPENNESS),"ojoI");
 
         addMessage("/gesture/mouth/width", tracker.getGesture(ofxFaceTracker::MOUTH_WIDTH));
         //addMessage("/gesture/mouth/height", tracker.getGesture(ofxFaceTracker::MOUTH_HEIGHT));
@@ -75,18 +81,44 @@ void FaceOsc::addMessage(string address, float data, string joint) {
 	if(joint=="altaD"){
 	    x= 3113.22-data;
         y= 1600- (data*70);
-        cout << "altaD-x: " << x <<endl;
-        cout << "altaD-y: " << y <<endl<<endl;
 	}
 	if(joint=="altaI"){
 	    x= 2394.99+data;
         y= 1600- (data*70);
-        //cout << "altaI-x: " << x<<endl;
-        //cout << "altaI-y: " << y<<endl<<endl;
 	}
 	if(joint=="nariz"){
-	    x= 2782.02;
-        y= 1807.62- (data*20);
+	    x= 2781.97;
+        y= 1600 + (data*30);
+	}
+
+	//panda
+	if(joint=="boquita"){
+	    x= 1715;
+        y= 1770 + (data*20);
+	}
+	if(joint=="ojoI"){
+	    x= 1540;
+	    if(data>2.9){
+            y= 1185;
+	    }else{
+            y=1326;
+	    }
+	}
+	if(joint=="ojoD"){
+	    x= 1897;
+        if(data>2.9){
+            y= 1185;
+	    }else{
+            y=1326;
+	    }
+	}
+	if(joint=="orejaD"){
+	    x= 1900 + (data*10);
+        y= 1100- (data*30);
+	}
+	if(joint=="orejaI"){
+	    x= 1518.31 - (data*10);
+        y= 1100- (data*30);
 	}
 	msg.addFloatArg(x);
 	msg.addFloatArg(y);
